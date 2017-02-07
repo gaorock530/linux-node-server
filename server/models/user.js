f292a1c816b4980992ac1170e0d4f45f31d8a477
gaorock530@hotmail.com <gaorock530@hotmail.com>
2017-02-08 01:29:57 +0800

    add tokens

--- server/models/user.js
+++ server/models/user.js
@@ -1,21 +1,53 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-const User = mongoose.model('User', {
+var userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 3
+    minlength: 3,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{value} is not valid email address'
+    }
   },
   password: {
     type: String,
     required: true,
-    minlength: 10
+    minlength: 8
   },
-  createdAt: {
-    type: Number,
-    default: 123456
-  }
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
+
+userSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+userSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'magic').toString();
+  user.tokens.push({access, token});
+  return user.save().then(() => {
+    return token;
+  });
+}
+
+var User = mongoose.model('User', userSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const _ = require('lodash');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo.js');
 const {User} = require('./models/user.js');
+const {ObjectId} = require('mongodb');
 
 var app = express();
 
@@ -12,9 +13,13 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('hello');
 });
-
+/*
+*---------------------
+* Todo App
+*---------------------
+*/
+//add Todos
 app.post('/todos', (req, res) => {
-  //console.log(req.body);
   var todo = new Todo({
     text: req.body.text
   });
@@ -26,12 +31,101 @@ app.post('/todos', (req, res) => {
   });
 });
 
+//fatching Todos
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+//GET /todos/12123123
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  if (!ObjectId.isValid(id)) return res.status(404).send('ID is not valid!');
+  Todo.findById(id).then((todo) => {
+    if (!todo) return res.send('Unable to find');
+    res.send(JSON.stringify(todo, undefined, 2));
+  }, (e) => {
+    console.log(e);
+    res.send('Server error');
+  });
+});
+
+//Delete Todos
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectId.isValid(id)) return res.status(404).send('ID is not valid!');
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) return res.status(404).send('Unable to find');
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+//Update Todos
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+  if (!ObjectId.isValid(id)) return res.status(404).send('ID is not valid!');
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) return res.status(404).send('Unable to update');
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+/*
+*---------------------
+* User App
+*---------------------
+*/
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(`Check email or password again! ${e}`);
+  });
+
+});
+
+app.get('/users', (req,res) => {
+  User.find().then((users) => {
+    res.send({users});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/users/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectId.isValid(id)) return res.status(400).send('Invalid ID!');
+  User.findById(id).then((user) => {
+    if (!user) return res.status(404).send('Unable to find user!');
+    res.send({user});
+  }).catch((e) => {
+    res.status(400).send('Server error!');
+  });
+});
+
+app.delete('/users/:id', (req, res) => {
+
+});
+
+app.patch('/user/:id', (req, res) => {
+
 });
 
 app.listen(3000, (con) => {
